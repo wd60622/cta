d06c9c090712d0349f1ec1405ad49947d3e9c9a6
Will Dean <william.dean@free-now.com>
2023-03-04 11:53:26 +0100

    add additional classifiers

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11", 
+        "Typing :: Typed", 
     ],
     packages=["cta"],
     install_requires=["requests", "pandas"],
